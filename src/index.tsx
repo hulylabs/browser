77a4be035832404b2a80f056d4efe3f1b3ed1d55
NikitaSkrynnik <nikita.skrynnik@xored.com>
2025-04-28 15:16:28 +1100

    remove default context menu
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,9 @@
 import { render } from "solid-js/web";
 import AppComponent from "./App";
 
-
+if (!import.meta.env.DEV) {
+    document.oncontextmenu = (event) => {
+        event.preventDefault()
+    }
+}
 render(() => <AppComponent />, document.getElementById("root") as HTMLElement);
