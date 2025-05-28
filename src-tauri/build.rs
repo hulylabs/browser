7d2dda413d56e0581ac9a98cd3f38e7ceef873bb
NikitaSkrynnik <nikita.skrynnik@xored.com>
2025-05-29 09:49:32 +1100

    minor fixes
    Signed-off-by: NikitaSkrynnik <nikita.skrynnik@xored.com>

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,9 +1,7 @@
 use anyhow::Result;
 
-use std::{
-    fs:: File,
-    path::Path,
-};
+use std::{fs::File, path::Path};
+
 #[cfg(target_os = "linux")]
 const HULY_CEF_PATH: &str = "cef/linux-x86_64.zip";
 #[cfg(target_os = "macos")]
@@ -13,7 +11,7 @@ const HULY_CEF_PATH: &str = "cef/windows-x86_64.zip";
 
 fn extract(filepath: &Path) -> Result<()> {
     let file = File::open(filepath)?;
-    zip::ZipArchive::new(file)?.extract( filepath.parent().unwrap())?;
+    zip::ZipArchive::new(file)?.extract(filepath.parent().unwrap())?;
 
     Ok(())
 }
