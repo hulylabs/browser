13bccfb3880c4a3fa8f5b74db576d0d455e03067
Nikita Skrynnik <nikita.skrynnik@xored.com>
2025-09-17 19:50:45 +0300

    add cool styles and history
    Signed-off-by: Nikita Skrynnik <nikita.skrynnik@xored.com>

--- src/components/sidebar/NewTabInput.tsx
+++ src/components/sidebar/NewTabInput.tsx
@@ -1,11 +1,22 @@
 import { Portal } from "solid-js/web";
 import { AppState } from "../../state/state";
-import { createSignal, onMount } from "solid-js";
+import { createSignal, onMount, For } from "solid-js";
 import styles from "./NewTabInput.module.scss"
+import { HistoryIcon, SearchIcon } from "lucide-solid";
+import { Separator } from "@kobalte/core/separator";
 
 export function NewTabInput(props: { app: AppState, close: () => void }) {
     const [input, setInput] = createSignal("");
     let inputRef!: HTMLInputElement;
+
+    const searchHistory = [
+        "github.com",
+        "stackoverflow.com",
+        "developer.mozilla.org",
+        "typescript handbook",
+        "solid-js documentation",
+        "css grid tutorial",
+    ];
 
     onMount(() => inputRef?.focus());
 
@@ -19,19 +30,39 @@ export function NewTabInput(props: { app: AppState, close: () => void }) {
         }
     }
 
+    const handleHistoryClick = (historyItem: string) => {
+        props.app.newTab(historyItem);
+        props.close();
+    }
+
     return <Portal>
         <div class={styles.overlay} onClick={() => props.close()}>
-            <input
-                ref={inputRef}
-                type="text"
-                value={input()}
-                placeholder="Search..."
-                onKeyDown={onKeyDown}
-                onClick={e => e.stopPropagation()}
-                onInput={e => setInput(e.currentTarget.value)}
-                autofocus
-                class={styles.input}
-            />
+            <div class={styles.searchContainer} onClick={e => e.stopPropagation()}>
+                <div class={styles.inputContainer}>
+                    <SearchIcon size={24} class={styles.searchIcon} />
+                    <input
+                        ref={inputRef}
+                        type="text"
+                        value={input()}
+                        placeholder="Search..."
+                        onKeyDown={onKeyDown}
+                        onInput={e => setInput(e.currentTarget.value)}
+                        autofocus
+                        class={styles.input}
+                    />
+                </div>
+                <Separator class={styles.separator} />
+                <div class={styles.historyContainer}>
+                    <For each={searchHistory}>
+                        {(item) => (
+                            <div class={styles.historyItem} onClick={() => handleHistoryClick(item)}>
+                                <HistoryIcon size={20} class={styles.historyIcon} />
+                                <span class={styles.historyText}>{item}</span>
+                            </div>
+                        )}
+                    </For>
+                </div>
+            </div>
         </div>
     </Portal>
 }
